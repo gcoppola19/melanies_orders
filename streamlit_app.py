1ba4127bbbcad993995dbe4c81eead4fed271969
Grace Coppola <112916407+gcoppola19@users.noreply.github.com>
2026-08-17 17:46:03 -0500

    Create Streamlit app for pending smoothie orders
    Implement a Streamlit app to manage pending smoothie orders with Snowflake integration.

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app
+st.title(f":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(
+  """Orders that need to be filled
+  """
+)
+
+cns = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.orders")
+
+if my_dataframe:
+    #st.dataframe(data=my_dataframe, use_container_width=True)
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        try:
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+            st.success("Order(s) updated!", icon='👍')
+        except:
+            st.write("Something went wrong.")
